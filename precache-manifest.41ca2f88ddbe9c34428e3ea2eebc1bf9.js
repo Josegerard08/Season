773220d440c431d11e43b8df44f00956787c6162
Jose Diaz <josediaz@Joses-Air-3.attlocal.net>
2020-08-03 22:30:11 -0500

    Updates

---
+++ precache-manifest.41ca2f88ddbe9c34428e3ea2eebc1bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57a025a574f7bca1194870dc39b8ab5",
+    "url": "/Season/index.html"
+  },
+  {
+    "revision": "9c9f9bab4e32565ea0ae",
+    "url": "/Season/static/js/2.dc6a5755.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Season/static/js/2.dc6a5755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c64fdd25ecba2d39be",
+    "url": "/Season/static/js/main.4191642a.chunk.js"
+  },
+  {
+    "revision": "e09f531e69059b4fed26",
+    "url": "/Season/static/js/runtime-main.5030ee5d.js"
+  }
+]);
